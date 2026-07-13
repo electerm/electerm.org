299484edf5ec0c2002cb912076d58dab59dba8c5
ZHAO Xudong <zxdong@gmail.com>
2026-07-13 09:17:29 +0800

    Fix

--- bin/fetch-release-info.js
+++ bin/fetch-release-info.js
@@ -48,7 +48,7 @@ async function main () {
   console.log('Fetching latest release info from GitHub...')
   const releaseInfo = await fetchReleaseInfo()
   if (releaseInfo.release.body) {
-    releaseInfo.release.body = releaseInfo.release.body.replace(/\r\n\r\n-------\r\n\r\nDownload下载: \[https:\/\/electerm\.html5beta\.com\]\(https:\/\/electerm\.html5beta\.com\)$/, '')
+    releaseInfo.release.body = releaseInfo.release.body.replace(/[\r\n]+-{3,}[\r\n]+Download下载: \[https:\/\/electerm\.html5beta\.com\]\(https:\/\/electerm\.html5beta\.com\)\s*$/, '')
   }
   console.log('Fetching repository info from GitHub...')
   const repoInfo = await fetchRepoInfo()
