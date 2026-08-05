7d4c133f61ff292f34522222e59234843d18c05c
ZHAO Xudong <zxdong@gmail.com>
2026-08-05 14:11:54 +0800

    Update info, add harmony related

--- bin/data.js
+++ bin/data.js
@@ -346,7 +346,7 @@ function createReleaseData () {
 }
 
 export default {
-  desc: 'Free and open-sourced terminal/ssh/sftp/telnet/serialport/RDP/VNC/Spice/ftp client (linux, mac, win, android)',
+  desc: 'Free and open-sourced terminal/ssh/sftp/telnet/serialport/RDP/VNC/Spice/ftp client (linux, mac, win, android, harmony)',
   siteName: 'electerm',
   host: process.env.HOST,
   videos: videos.videos,
@@ -464,7 +464,9 @@ export default {
         { title: 'electerm AI — Free AI for electerm users', url: 'https://ai.electerm.org', external: true },
         { title: 'electerm theme — Create/share theme site with live preview and AI creation', url: 'https://theme.electerm.org', external: true },
         { title: 'AI Dream', url: 'https://ai-dream.html5beta.com', external: true },
-        { title: 'electerm-android', url: 'https://github.com/electerm/electerm-android', external: true }
+        { title: 'electerm-android', url: 'https://github.com/electerm/electerm-android', external: true },
+        { title: 'electerm-harmony (HarmonyOS)', url: 'https://github.com/electerm/electerm-harmony', external: true },
+        { title: 'electerm on AppGallery (HarmonyOS)', url: 'https://appgallery.huawei.com/app/detail?id=org.electerm.electerm', external: true }
       ]
     },
     {
